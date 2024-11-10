ac3108a1346526580a7d82608beec7ebcb152e11
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2024-11-10 05:03:37 +0100

    added style to landing page, sign in and log in page

--- culturo-link-react/cultureLink/src/pages/LandingPage.jsx
+++ culturo-link-react/cultureLink/src/pages/LandingPage.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import "../css/LandingPage.css";
 import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
-    <div>
-      <header>
-        <h1>CultureLink</h1>
-      </header>
-      <section>
-        <button>
-          <Link to="/signup-login">SignUp/Login</Link>
-        </button>
-        <button>
-          <Link to="/dashboard">Dashboard</Link>
-        </button>
-        <button>
-          <Link to="/explore">Explore</Link>
-        </button>
-      </section>
+    <div className="LandingMainBackground">
+      <div className="LandingMainContent">
+        <header>
+          <h1 className="LandingHeaderText">CultureLink</h1>
+        </header>
+        <section className="LandingCallToAction">
+          <button>
+            <Link to="/signup-login">SignUp/Login</Link>
+          </button>
+          <button>
+            <Link to="/dashboard">Dashboard</Link>
+          </button>
+          <button>
+            <Link to="/explore">Explore</Link>
+          </button>
+        </section>
+      </div>
     </div>
   );
 }
--- culturo-link-react/cultureLink/src/pages/SignUpLoginPage.jsx
+++ culturo-link-react/cultureLink/src/pages/SignUpLoginPage.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/SignUpLoginPage.css";
 
 function SignUpLoginPage() {
   return (
-    <div>
-      <h2>Sign Up / Log In</h2>
-      <div>
-        <h3>Sign Up</h3>
-        <Link to="/dashboard">Create Account</Link>
-      </div>
-      <div>
-        <h3>Log In</h3>
-        <Link to="/dashboard">Log In</Link>
+    <div className="SignUpLoginContent">
+      <h1>CultureLink</h1>
+      <p>
+        Join us in empowering global understanding. Let’s make culture a shared
+        journey, not an isolated experience.
+      </p>
+      <div className="ContentCardContainer">
+        <div className="ContentCard">
+          <h2>Rejoin the Adventure</h2>
+          <p>Unlock your next step in global connection.</p>
+          <button>
+            <Link to="/log-in">Log In</Link>
+          </button>
+        </div>
+        <div className="ContentCard">
+          <h2>Join the Global Tribe</h2>
+          <p>Embark on a journey to discover and celebrate cultures.</p>
+          <button>
+            <Link to="/sign-in">Create Account</Link>
+          </button>
+        </div>
       </div>
     </div>
   );
